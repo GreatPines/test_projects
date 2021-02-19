44bd1482b120de938dcf88da136833bbc67d49b3
Ian <ianrogers2422@gmail.com>
2021-02-18 19:46:53 -0500

    New webpage

---
+++ Test/app.js
@@ -0,0 +1,11 @@
+function reverseString(str){
+    const strArr = str.split('');
+    strArr.reverse();
+
+    console.log(strArr);
+}
+
+const output = reverseString('hello');
+
+console.log(output);
+
